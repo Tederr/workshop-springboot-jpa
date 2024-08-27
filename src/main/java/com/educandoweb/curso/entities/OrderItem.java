5725b64442966f31dbc0e1a4f82429dd8e9e7038
Joao Pedro <jp3351207@gmail.com>
2024-08-27 18:19:04 -0300

    Entidade OrderItem. Associação muitos-para-muitos com dados extras - PARTE 2

--- src/main/java/com/educandoweb/curso/entities/OrderItem.java
+++ src/main/java/com/educandoweb/curso/entities/OrderItem.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import com.educandoweb.curso.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -16,7 +17,7 @@ public class OrderItem implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -25,12 +26,14 @@ public class OrderItem implements Serializable{
 	}
 
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		super();
 		id.setOrder(order);
 		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
@@ -39,7 +42,7 @@ public class OrderItem implements Serializable{
 		id.setOrder(order); 
 	}
 	
-	public Product product () {
+	public Product getProduct () {
 		return id.getProduct();
 	}
 	
