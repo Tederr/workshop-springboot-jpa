0a26f76856460c15bb86e124c7f66176947201a2
Joao Pedro <jp3351207@gmail.com>
2024-08-27 15:46:05 -0300

    Entidade Category

--- src/main/java/com/educandoweb/curso/entities/Order.java
+++ src/main/java/com/educandoweb/curso/entities/Order.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 
 import com.educandoweb.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
